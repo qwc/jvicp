d22104cb1758b7297672957abc5999b372de49d2
qwc <otte.mm@googlemail.com>
2013-07-31 15:28:17 +0200

    some commentation in the test class

--- src/at/mkwe/jvicp/lecroy/LecroyTest.java
+++ src/at/mkwe/jvicp/lecroy/LecroyTest.java
@@ -11,9 +11,13 @@ public class LecroyTest {
   public static void main(String[] args) {
     JVICP vicp = new JVICP();
     try {
+      // connect to lecroy waveace 2002
       vicp.connect("192.168.0.5");
+      // send identification query
       vicp.sendDataToDevice("*IDN?", true);
+      // read identification
       VICPData data = vicp.readDataFromDevice();
+      // fire it out on the console
       System.out.println(data);
     } catch (UnknownHostException e) {
       e.printStackTrace();
